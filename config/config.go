08b7c386449478958f47669004cf05ffe79117fc
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 09:23:30 +0300

    refactoring

--- config/config.go
+++ config/config.go
@@ -9,6 +9,6 @@ var (
 	ServiceHost = "0.0.0.0"
 	ServicePort = args.GetString("service_port", "8080")
 
-	RegistryHost = env.GetVar("REGISTRY_HOST", "127.0.0.1")
-	RegistryPort = env.GetVar("REGISTRY_PORT", "8500")
+	DiscoveryHost = env.GetVar("SIMPLE_MUSIC_CONSUL_HOST", "127.0.0.1")
+	DiscoveryPort = env.GetVar("SIMPLE_MUSIC_CONSUL_PORT", "8500")
 )
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/simple-music/gateway/config"
+	"github.com/simple-music/gateway/logs"
+	"github.com/simple-music/gateway/utils"
+)
 
+func main() {
+	logger := logs.NewLogger()
+
+	_ = utils.NewDiscoveryClient(
+		utils.DiscoveryClientConfig{
+			DiscoveryServerHost: config.DiscoveryHost,
+			DiscoveryServerPort: config.DiscoveryPort,
+		},
+		utils.DiscoveryClientComponents{
+			Logger: logger,
+		},
+	)
 }
