56a4ffcabd5f977032043ab6683caaa44980a497
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 09:17:13 +0300

    rest client basic implementation added

--- utils/restClient.go
+++ utils/restClient.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"github.com/simple-music/gateway/errs"
 	"github.com/simple-music/gateway/logs"
 	"github.com/valyala/fasthttp"
 )
@@ -10,7 +12,8 @@ type RestClientConfig struct {
 }
 
 type RestClientComponents struct {
-	Logger *logs.Logger
+	Logger          *logs.Logger
+	DiscoveryClient *DiscoveryClient
 }
 
 type RestClient struct {
@@ -27,4 +30,21 @@ func NewRestClient(cf RestClientConfig, cp RestClientComponents) *RestClient {
 			Name: cf.ServiceName + "-client",
 		},
 	}
+}
+
+func (c *RestClient) PerformRequest(req *fasthttp.Request, path string) (*fasthttp.Response, *errs.Error) {
+	address, err := c.components.DiscoveryClient.ResolveName(c.config.ServiceName)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetRequestURI(fmt.Sprintf("http://%s%s", address, path))
+
+	resp := fasthttp.AcquireResponse()
+	reqErr := c.client.Do(req, resp)
+	if reqErr != nil {
+		return nil, errs.NewServiceError(reqErr)
+	}
+
+	return resp, nil
 }
