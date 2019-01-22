4c198efef32eff8c9a7aa756269846c062b6cc5d
denis96z <zinoviev.denis@yandex.ru>
2019-01-22 11:06:23 +0300

    avatars handlers implemented

--- rest/avatarsHandlers.go
+++ rest/avatarsHandlers.go
@@ -1,12 +1,19 @@
 package rest
 
 import (
+	"github.com/simple-music/gateway/models"
 	"github.com/valyala/fasthttp"
 )
 
 func (srv *Service) addAvatar(ctx *fasthttp.RequestCtx) {
 	user := ctx.UserValue("user").(string)
 	content := ctx.Request.Body()
+
+	var musician models.Musician
+	if err := srv.musiciansClient.GetMusician(user, &musician); err != nil {
+		srv.WriteError(ctx, err)
+		return
+	}
 
 	err := srv.avatarsClient.AddAvatar(user, content)
 	if err != nil {
@@ -18,9 +25,26 @@ func (srv *Service) addAvatar(ctx *fasthttp.RequestCtx) {
 }
 
 func (srv *Service) getAvatar(ctx *fasthttp.RequestCtx) {
-	//TODO
+	user := ctx.UserValue("user").(string)
+
+	content, err := srv.avatarsClient.GetAvatar(user)
+	if err != nil {
+		srv.WriteError(ctx, err)
+		return
+	}
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetContentType("image/jpeg")
+	ctx.SetBody(content)
 }
 
 func (srv *Service) deleteAvatar(ctx *fasthttp.RequestCtx) {
-	//TODO
+	user := ctx.UserValue("user").(string)
+
+	if err := srv.avatarsClient.DeleteAvatar(user); err != nil {
+		srv.WriteError(ctx, err)
+		return
+	}
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
 }
