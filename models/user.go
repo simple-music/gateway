42b5be295c812b87eb307dc835553f3e81bcf2fd
denis96z <zinoviev.denis@yandex.ru>
2019-01-22 14:34:48 +0300

    validators added

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/simple-music/gateway/errs"
+	"regexp"
+)
+
 //go:generate easyjson
 
 const (
@@ -68,4 +73,79 @@ func (v *NewCredentials) From(newUser *NewUser, musician *Musician) {
 type AuthCredentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type NewUserValidator struct {
+	usernameRegexp *regexp.Regexp
+	emailRegexp    *regexp.Regexp
+	fullNameRegexp *regexp.Regexp
+
+	usernameErr *errs.Error
+	passwordErr *errs.Error
+	emailErr    *errs.Error
+	fullNameErr *errs.Error
+}
+
+func NewNewUserValidator() *NewUserValidator {
+	return &NewUserValidator{
+		usernameRegexp: regexp.MustCompile(UsernamePattern),
+		emailRegexp:    regexp.MustCompile(EmailPattern),
+		fullNameRegexp: regexp.MustCompile(FullNamePattern),
+
+		usernameErr: errs.NewError(errs.InvalidFormat, "invalid username"),
+		passwordErr: errs.NewError(errs.InvalidFormat, "invalid password"),
+		emailErr:    errs.NewError(errs.InvalidFormat, "invalid email"),
+		fullNameErr: errs.NewError(errs.InvalidFormat, "invalid full name"),
+	}
+}
+
+func (v *NewUserValidator) Validate(user *NewUser) *errs.Error {
+	if !v.usernameRegexp.MatchString(user.Username) {
+		return v.usernameErr
+	}
+	if len(user.Password) < PasswordMinLength {
+		return v.passwordErr
+	}
+	if !v.emailRegexp.MatchString(user.Email) {
+		return v.emailErr
+	}
+	if !v.fullNameRegexp.MatchString(user.FullName) {
+		return v.fullNameErr
+	}
+	return nil
+}
+
+type MusicianUpdateValidator struct {
+	emailRegexp    *regexp.Regexp
+	fullNameRegexp *regexp.Regexp
+	dateRegexp     *regexp.Regexp
+
+	emailErr       *errs.Error
+	fullNameErr    *errs.Error
+	dateOfBirthErr *errs.Error
+}
+
+func NewMusicianUpdateValidator() *MusicianUpdateValidator {
+	return &MusicianUpdateValidator{
+		emailRegexp:    regexp.MustCompile(EmailPattern),
+		fullNameRegexp: regexp.MustCompile(FullNamePattern),
+		dateRegexp:     regexp.MustCompile(DatePattern),
+
+		emailErr:       errs.NewError(errs.InvalidFormat, "invalid email"),
+		fullNameErr:    errs.NewError(errs.InvalidFormat, "invalid full name"),
+		dateOfBirthErr: errs.NewError(errs.InvalidFormat, "invalid date of birth"),
+	}
+}
+
+func (v *MusicianUpdateValidator) Validate(update *MusicianUpdate) *errs.Error {
+	if update.Email != nil && !v.emailRegexp.MatchString(*update.Email) {
+		return v.emailErr
+	}
+	if update.FullName != nil && !v.fullNameRegexp.MatchString(*update.FullName) {
+		return v.fullNameErr
+	}
+	if update.DateOfBirth != nil && !v.dateRegexp.MatchString(*update.DateOfBirth) {
+		return v.dateOfBirthErr
+	}
+	return nil
 }
